be55b4bb2e92b2aebef0205966f5b213691a1004
EduardGuilherme <eduguilherme48@gmail.com>
2024-02-14 21:49:02 -0300

    initial commit

---
+++ acheiprecoApi/src/main/java/br/com/acheipreco/acheiprecoApi/AcheiprecoApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.acheipreco.acheiprecoApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcheiprecoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcheiprecoApiApplication.class, args);
+	}
+
+}
---
+++ acheiprecoApi/src/test/java/br/com/acheipreco/acheiprecoApi/AcheiprecoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.acheipreco.acheiprecoApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcheiprecoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
