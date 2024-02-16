cef5c64083dbcd62515fefab71a5d2f4fa9ec993
EduardGuilherme <eduguilherme48@gmail.com>
2024-02-15 23:20:38 -0300

    update repository

--- acheiprecoApi/src/main/java/br/com/acheipreco/acheiprecoApi/Repository/UserRepository.java
+++ acheiprecoApi/src/main/java/br/com/acheipreco/acheiprecoApi/Repository/UserRepository.java
@@ -1,4 +1,11 @@
 package br.com.acheipreco.acheiprecoApi.Repository;
 
-public interface UserRepository {
+import br.com.acheipreco.acheiprecoApi.Model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+    UserDetails findByEmail(String email);
 }
