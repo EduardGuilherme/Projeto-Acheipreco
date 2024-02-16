96309b1cbb9d76e202043635f0930071d8df15c8
EduardGuilherme <eduguilherme48@gmail.com>
2024-02-15 23:20:48 -0300

    update user

--- acheiprecoApi/src/main/java/br/com/acheipreco/acheiprecoApi/Model/User.java
+++ acheiprecoApi/src/main/java/br/com/acheipreco/acheiprecoApi/Model/User.java
@@ -2,15 +2,24 @@ package br.com.acheipreco.acheiprecoApi.Model;
 
 import br.com.acheipreco.acheiprecoApi.Enums.Tipodeusuario;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 @EntityScan
+@Getter
+@Setter
+@NoArgsConstructor
 @Table(name = "user")
-public class User {
+public class User implements UserDetails {
     private static final long SerialVersion = 1L;
 
     @Id
@@ -23,59 +32,56 @@ public class User {
     @Enumerated(EnumType.ORDINAL)
     private Tipodeusuario tipodeusuario;
 
-    public User() {
-    }
 
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        // Colocando um comentario pos possivelmente vamos precisar desse bloco de código
+        if(this.tipodeusuario == Tipodeusuario.EMPRESA){
+            return List.of(new SimpleGrantedAuthority("EMPRESA"));
+        }else if(this.tipodeusuario == Tipodeusuario.CLIENTE){
+            return List.of(new SimpleGrantedAuthority("CLIENTE"));
+        }else{
+            return List.of(new SimpleGrantedAuthority("ADMIN"),
+                    new SimpleGrantedAuthority("EMPRESA"),
+                    new SimpleGrantedAuthority("CLIENTE"));
+        }
+    }
+
+    public User(String nomeCompleto, String nomeEmpresa, String senha, String email, Tipodeusuario tipodeusuario) {
+        this.nomeCompleto = nomeCompleto;
+        this.nomeEmpresa = nomeEmpresa;
+        this.senha = senha;
+        this.email = email;
+        this.tipodeusuario = tipodeusuario;
+    }
+
+    @Override
+    public String getPassword() {
         return null;
     }
 
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public String getNomeCompleto() {
-        return nomeCompleto;
-    }
-
-    public void setNomeCompleto(String nomeCompleto) {
-        this.nomeCompleto = nomeCompleto;
-    }
-
-    public String getNomeEmpresa() {
-        return nomeEmpresa;
-    }
-
-    public void setNomeEmpresa(String nomeEmpresa) {
-        this.nomeEmpresa = nomeEmpresa;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
-
-    public String getEmail() {
+    @Override
+    public String getUsername() {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
     }
 
-    public Tipodeusuario getTipodeusuario() {
-        return tipodeusuario;
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
     }
 
-    public void setTipodeusuario(Tipodeusuario tipodeusuario) {
-        this.tipodeusuario = tipodeusuario;
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
     }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+
 }
