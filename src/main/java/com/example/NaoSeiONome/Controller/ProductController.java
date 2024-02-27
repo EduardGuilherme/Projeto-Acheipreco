ea87d66548124aa7a0e1a204b47fc4a62fab452c
JoaoVR-Dev <joaovr.dev@outlook.com>
2024-02-26 23:04:33 -0300

    Inserindo tabela produto e suas funções

---
+++ src/main/java/com/example/NaoSeiONome/Controller/ProductController.java
@@ -0,0 +1,39 @@
+
+package com.example.NaoSeiONome.Controller;
+
+import com.example.NaoSeiONome.DTO.ProductDTO;
+import com.example.NaoSeiONome.Services.ProductService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("product")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("/list")
+    public ResponseEntity findAll(){
+        return null;//productService.listAllProduct();
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity create(@RequestBody @Valid ProductDTO productDTO){
+        return productService.createProduct(productDTO);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity delete(@PathVariable String id){
+        return productService.deleteProduct(id);
+    }
+
+    @PutMapping("/change/{id}")
+    public ResponseEntity change(@RequestBody @Valid ProductDTO productDTO, @PathVariable String id){
+        return productService.changeProduct(productDTO,id);
+    }
+}
+
+
---
+++ src/main/java/com/example/NaoSeiONome/DTO/ProductDTO.java
@@ -0,0 +1,7 @@
+
+package com.example.NaoSeiONome.DTO;
+
+import java.math.BigDecimal;
+
+public record ProductDTO(String idCompany, String name, String type, String image, Integer stack, BigDecimal price) {
+}
